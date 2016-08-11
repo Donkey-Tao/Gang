d4ef44c31ff4c3c5391f839191964f60165389e6
Fei Tao <taofei@localhost>
2016-08-11 17:30:36 +0800

    Init

---
+++ 205.Gang/205.Gang/Classes/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  205.Gang
+//
+//  Created by 陶飞 on 16/8/11.
+//  Copyright © 2016年 TaoFei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
